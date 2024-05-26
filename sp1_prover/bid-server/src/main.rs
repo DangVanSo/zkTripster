742c35f33d79c771bb5a0fc37264814fdeee336e
Adam Gordon <3723289+gordon-to@users.noreply.github.com>
2024-05-26 00:57:44 +0000

    cors

--- sp1_prover/bid-server/src/main.rs
+++ sp1_prover/bid-server/src/main.rs
@@ -1,18 +1,29 @@
 #[macro_use] extern crate rocket;
 
 use rocket::{Rocket, Build, State};
+use rocket::http::Method;
+
+use rocket::serde::Serialize;
+
+use rocket::serde::json::Json;
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Guard, Responder};
 use clap::Parser;
 
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[derive(Debug, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Resp {
+    proof: String,
+    address: String,
 }
 
 // return contets of proof
 #[get("/proof")]
-async fn proof(args: &State<Args>) -> String {
-    args.proof.clone()
+async fn proof(cors: Guard<'_>, args: &State<Args>) -> Responder<Json<Resp>> {
+    let resp = Resp {
+        proof: args.proof.clone(),
+        address: args.address.clone(),
+    };
+    cors.responder(Json(resp))
 }
 
 /// Simple program to serve proof files
@@ -30,13 +41,21 @@ struct Args {
 
 #[launch]
 fn rocket() -> Rocket<Build> {
+
+    let allowed_origins = AllowedOrigins::some_exact(&["https://zktripster.pages.dev"]);
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors().unwrap();
+
     let args = Args::parse();
     rocket::build()
-        // .mount("/", routes![hello, hello]) // uncomment this to get an error
-        // .mount("/", routes![unmanaged]) // uncomment this to get a sentinel error
-        .mount("/", routes![index])
         .mount("/", routes![proof])
+        .mount("/", rocket_cors::catch_all_options_routes())
         .manage(args)
+        .manage(cors)
 }
-
-
