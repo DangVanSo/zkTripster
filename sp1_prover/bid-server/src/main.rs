e4f71d3f39520ee14ba0d1660e988157605b4e77
Adam Gordon <3723289+gordon-to@users.noreply.github.com>
2024-05-26 00:09:24 +0000

    add proof server

--- sp1_prover/bid-server/src/main.rs
+++ sp1_prover/bid-server/src/main.rs
@@ -1,38 +1,42 @@
 #[macro_use] extern crate rocket;
 
-use rocket::{Rocket, Build};
+use rocket::{Rocket, Build, State};
+use clap::Parser;
+
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+// return contets of proof
+#[get("/proof")]
+async fn proof(args: &State<Args>) -> String {
+    args.proof.clone()
+}
+
+/// Simple program to serve proof files
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// path to proof
+    #[arg(short, long)]
+    proof: String,
+
+    /// redemption address
+    #[arg(short, long, default_value_t = (&"0x..").to_string())]
+    address: String,
+}
+
+#[launch]
 fn rocket() -> Rocket<Build> {
+    let args = Args::parse();
     rocket::build()
         // .mount("/", routes![hello, hello]) // uncomment this to get an error
         // .mount("/", routes![unmanaged]) // uncomment this to get a sentinel error
         .mount("/", routes![index])
-}
-
-#[derive(Parser)]
-#[group(skip)]
-struct Options {
-    /// File to read
-    #[clap(long, env, default_value = "")]
-    proofPath: PathBuf,
+        .mount("/", routes![proof])
+        .manage(args)
 }
 
 
-async fn app(options: Options) -> Result<()> {
-    let mut proof = File::open(options.proofPath).await?;
-    // verify
-    Ok(())
-}
-
-fn main() {
-    if let Err(e) = rocket().launch().await {
-        println!("Whoops! Rocket didn't launch!");
-        // We drop the error to get a Rocket-formatted panic.
-        drop(e);
-    };
-}
